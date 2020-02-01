231adcb0923ca2f5469372cb3550ec53d4af2ed2
villatrue <scarter0313@gmail.com>
2020-02-01 00:52:58 -0500

    first commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,18 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Pacific Pool Service</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <h4>
+                The owner Jose Reyes who has worked in the pool industry since 1993, started pacific pool service in 2002.
+                Jose fell in love with the trade, the connection that is established with the customer is unbelievable to him;
+                the freedom of being outdoors and solving problems. <br/>
+                <br/>
+                "I'm always learning something new because just like everything else, the industry is evolving."
+                <h4 className="align-center"> -Jose Reyes </h4>
+                </h4>
+                
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
@@ -18,3 +25,6 @@ const Banner = (props) => (
 )
 
 export default Banner
+
+
+
